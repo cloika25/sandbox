d16a8e573d3f01e831801a50299aa81820f345d0
cloika25 <cloika25@yandex.ru>
2025-01-03 18:37:01 +0300

    try fix gh

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "sandbox",
   plugins: [
     react(),
     eslint({ include: ["src/**/*.js", "src/**/*.vue", "src/**/*.ts"] }),
