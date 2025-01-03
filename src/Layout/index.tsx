21bae463ef0ae51863cac26322cbee9eec19d83c
cloika25 <cloika25@yandex.ru>
2025-01-03 18:22:35 +0300

    test2

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -1,5 +1,12 @@
-export const Layout = () => (
+import { ReactNode } from "react";
+
+type LayoutProps = {
+  children?: ReactNode;
+};
+
+export const Layout = ({ children }: LayoutProps) => (
   <div>
     <aside></aside>
+    <div>{children}</div>
   </div>
 );
