cf5fa3d5fa68c099b2290f63dba02c64b3bd3c87
cloika25 <cloika25@yandex.ru>
2025-02-22 13:44:34 +0300

    пробуем gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import eslint from "vite-plugin-eslint2";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/sandbox/",
-  plugins: [react(), eslint({
-    emitWarning: true,
-  })],
+  plugins: [
+    react(),
+    eslint({
+      emitWarning: true,
+    }),
+  ],
 });
